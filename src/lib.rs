21d1bacb7d68637a83d63a68798018dec6397f33
Paul Ollivier <contact@paulollivier.fr>
2019-06-25 23:20:15 +0200

    Add basic struct info; Fix typo in Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,27 @@
+use chrono::{Datelike, DateTime, Utc};
+use chrono::format::Fixed::TimezoneName;
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+}
+
+/// Structure holding all the info to represent a task item
+/// id may be auto-generated
+pub struct TodoEntry {
+    id: u32,
+    title: String,
+    desc: String,
+    due: DateTime<Utc>,
+    project: String,
+    tags: Vec<String>,
+}
+
+impl TodoEntry {
+    pub fn new() -> Self {
+        TodoEntry { id: 0, title: String::new(), desc: String::new(), due: Utc::now(), project: String::new(), tags: Vec::new() }
     }
 }
